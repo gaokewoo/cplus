603309695a05b47711fff1548fadbe4b5c2908d6
Gaoke Woo <gaokewoo@126.com>
2013-01-14 18:57:49 +0800

    add toString func

--- src/Config.cpp
+++ src/Config.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <cstdlib>
+#include <map>
 
 using namespace std;
 
@@ -15,10 +16,13 @@ class Config{
 
 		int getItems();
 
+		void toString();
+
 		string get(const string item, const string key);
 
 	private:
 		const string _fileName;
+		map< string,map<string,string> > _items;
 
 };
 
@@ -29,6 +33,7 @@ void Config::trim(string & str) {
 	str.erase(str.find_last_not_of("\r\t\n ")+1);
 }
 
+//ugly implemention, try to improve it
 void Config::initConfig(){
 	 ifstream fin;
 	 fin.open(_fileName.c_str(),ios_base::in);
@@ -39,11 +44,50 @@ void Config::initConfig(){
 		 exit(3);
 	 }
 
-	while(!fin.eof() ){
-		string tmp;
-		fin>>tmp;
-		trim(tmp);
-		!tmp.empty() && cout<<"config info:"<<tmp<<endl;
+    string item;
+	while(!fin.eof() && getline(fin, item) ){
+		trim(item);
+
+		//not process for empty or comments out lines
+		if( item.empty() ||(item[0] == '#') ){
+			continue;
+		}
+
+		//The begin with char '[' explain a new item
+	    if( item[0] == '[' ) {
+			item.erase(0,item.find_first_not_of("["));
+			item.erase(item.find_last_not_of("]")+1);
+			//cout<<item<<endl;
+			
+			//handle subitems
+			string subitem;
+			streampos pos;
+			map<string,string> tmp;
+			while( !fin.eof() ){
+				pos=fin.tellg();
+				getline(fin, subitem);
+				trim(subitem);
+
+				if( subitem.empty() || (subitem[0] == '#') ){
+					continue;
+				}
+				
+				if( subitem[0] == '[' ) {
+					fin.seekg(pos);
+					break;
+				}
+
+				string key = subitem.substr(0,subitem.find("="));
+				string value = subitem.substr(subitem.find("=")+1);
+
+				//cout<<"For item ["<<item<<"] key="<<key<<",value="<<value<<endl;
+
+				tmp.insert(map<string,string>::value_type(key,value)); 
+			}
+			_items[item]=tmp;
+
+		}
+
 	}
 
 
@@ -52,18 +96,73 @@ void Config::initConfig(){
 }
 
 int Config::getItems(){
+	int count=0;
+	map< string, map<string,string> >::const_iterator map_it = _items.begin();
 
-	return 0;
+	while(map_it != _items.end()) {
+		count++;
+		map_it++;
+	}
+	return count;
+}
+
+void Config::toString(){
+	map< string, map<string,string> >::const_iterator map_it = _items.begin();
+
+	while(map_it != _items.end()){
+		cout<<"item:"<<map_it->first<<endl;
+
+		map<string,string> tmp = _items[map_it->first];
+		map<string,string>::const_iterator map_it_sub = tmp.begin();
+		while(map_it_sub != tmp.end()){
+			cout<<"\t"<<"key:"<<map_it_sub->first<<endl;
+			cout<<"\t"<<"value:"<<map_it_sub->second<<endl;
+
+			map_it_sub++;
+		}
+		map_it++;
+	}
 }
 
 string Config::get(const string item, const string key){
+	map< string, map<string,string> >::const_iterator map_it = _items.begin();
+	string value="";
 
-	return "";
+	while(map_it != _items.end()){
+		if( map_it->first == item){
+			map<string,string> tmp = _items[map_it->first];
+			map<string,string>::const_iterator map_it_sub = tmp.begin();
+			while(map_it_sub != tmp.end()){
+				cout<<"key:"<<map_it_sub->first<<endl;
+				cout<<"value:"<<map_it_sub->second<<endl;
+
+				if(map_it_sub->first == key){
+					value = map_it_sub->second;
+				}
+				map_it_sub++;
+			}
+		}
+		map_it++;
+	}
+
+	if(value == ""){
+		cerr<<"no data found"<<endl;
+		exit(1);
+	}
+
+	return value;
 }
 int main(){
-	Config a("/home/acer/cplus/config.ini");
+	Config a("../config.ini");
 
 	a.initConfig();
+	cout<<"The number of config items:"<<a.getItems()<<endl;
+
+	cout<<"Get config:"<<a.get("config1","item1")<<endl;
+
+	cout<<"---------toString------------"<<endl;
+	a.toString();
+	
 
 	return 0;
 }
--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 //#include<sys/types.h>
 //#include<unistd.h>
-
+using namespace std;
 pthread_t ntid;
 
 void printids(const char *s){
@@ -21,17 +21,21 @@ void *thr_fn(void *arg){
   return ((void *)0);
 }
 
+enum ab {a=1,b=2,c=3,d,e,f};
+
+class A{
+	public:
+		void f(const int& a){cout<<"const int a"<<endl;}
+		void f(int &a){cout<<"int a"<<endl;}
+};
+
+typedef void (A::*PF)(const int &a);
 int main(){
-  int err;
 
-  err = pthread_create(&ntid,NULL,thr_fn,NULL);
-  if(err != 0){
-   printf("can't create thread: %d\n",(err));
-   return 1;
-  }
+  PF pl=&A::f;
 
-  printids("main thread:");
-  sleep(1);
-  
+  A a;
+  //cout<<&(a.*pl)<<endl;
+
   return 0;
 }
