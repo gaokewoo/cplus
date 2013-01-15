d2c16f2bd881ba89183707205c1538bd40b47224
Gaoke Woo <gaokewoo@126.com>
2013-01-15 14:25:56 +0800

    rename src dir

--- test.cpp
+++ test.cpp
@@ -1,41 +1,19 @@
 #include <iostream>
 #include <pthread.h>
 #include <stdio.h>
-//#include<sys/types.h>
-//#include<unistd.h>
+#include <vector>
 using namespace std;
-pthread_t ntid;
 
-void printids(const char *s){
-pid_t pid;
-  pthread_t tid;
-
-  pid = getpid();
-  tid = pthread_self();
-  printf("%s pid %u tid %u (0x%x)\n",s,(unsigned int)pid,(unsigned int)tid,(unsigned 
-int)tid);
-}
-
-void *thr_fn(void *arg){
-  printids("new thread:");
-  return ((void *)0);
-}
-
-enum ab {a=1,b=2,c=3,d,e,f};
-
-class A{
-	public:
-		void f(const int& a){cout<<"const int a"<<endl;}
-		void f(int &a){cout<<"int a"<<endl;}
-};
-
-typedef void (A::*PF)(const int &a);
 int main(){
 
-  PF pl=&A::f;
+    string s1="123";
+	string s2=s1+"456";//one new address to store s2
 
-  A a;
-  //cout<<&(a.*pl)<<endl;
+	cout<<"s1 addr:"<<&s1<<" value:"<<s1<<endl;
+	cout<<"s2 addr:"<<&s2<<" value:"<<s2<<endl;
+
+	s1=s1+"789";
+	cout<<"s1 addr:"<<&s1<<" value:"<<s1<<endl;//address has no change
 
   return 0;
 }
