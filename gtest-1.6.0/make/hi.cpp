197dbf3ba20b5c184f8ab0f143a88c6bb629ae32
gaokewoo <gaokewoo@126.com>
2013-01-24 23:18:42 +0800

    add a test code for using google test suite to do unit test

---
+++ gtest-1.6.0/make/hi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int test(){
+	return 1;
+}
+
---
+++ gtest-1.6.0/make/test_hi.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+int test();
+
+TEST(wugk_test,negative){
+	EXPECT_EQ(1,test());
+}
+
