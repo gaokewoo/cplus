a6ea45b1b360aa32052d99d52d2857a792b33007
gaokewoo <gaokewoo@126.com>
2013-01-26 23:24:17 +0800

    add string insert practice

---
+++ container/comp.sh
@@ -0,0 +1,1 @@
+g++ -I../gtest-1.6.0/include -g -Wall -Wextra -lpthread  ../lib/gtest_main.a string_test.cpp
--- container/string_test.cpp
+++ container/string_test.cpp
@@ -7,7 +7,6 @@ class string_test:public testing::Test{
 	protected:
 
 		virtual void SetUp(){
-			cout<<"----initialize string s----"<<endl;
 			s="abc";
 		}
 
@@ -17,12 +16,41 @@ class string_test:public testing::Test{
 
 };
 
-TEST_F(string_test,insert) {
-	EXPECT_EQ("abc",s);
-	EXPECT_EQ("ababc",s.insert(0,"aa"));
+TEST_F(string_test,assign) {
+	string s2("abc",2);
+	EXPECT_EQ(s2,"ab");
+
+	string s3(s,2);
+	EXPECT_EQ(s3,"c");
+
+	string s4(s,2,2);
+	EXPECT_EQ(s4,"c");
 
 }
-TEST_F(string_test,insert2) {
-	EXPECT_EQ("aaabc",s.insert(0,"aa"));
+
+TEST_F(string_test,insert) {
+	string::iterator it=s.begin();
+	it++;
+
+	it=s.insert(it,'d');//the second element only support char type
+						//test s.insert(p,t)
+	EXPECT_EQ(s,"adbc");
+
+	s.insert(it,2,'e');//test s.insert(p,n,t),return void
+	EXPECT_EQ(s,"aeedbc");
+
+	string::iterator p=s.begin();
+	string::iterator b=it+1;
+	string::iterator e=it+2;
+	s.insert(p,b,e);
+	EXPECT_EQ(s,"eaeedbc");//test s.insert(p,b,e)
+
+	//ASSERT_EQ("ababc",s.insert(0,"aa"));
+	//EXPECT_EQ("ababc",s.insert(0,"aa"))<<"wugk test"<<endl;
+	s.insert(0,"aa");
+	EXPECT_EQ(s,"aaeaeedbc");//test s.insert(pos,cp)
+
+	s.insert(0,3,'f');
+	EXPECT_EQ(s,"fffaaeaeedbc");//test s.insert(pos,n,c)
 
 }
